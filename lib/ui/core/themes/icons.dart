28d1c522fa9d44918d522945220d34a802789305
alanegd <alangalavisd@gmail.com>
2025-04-28 01:12:25 -0500

    feat: Add custom back button

---
+++ lib/ui/core/themes/icons.dart
@@ -0,0 +1,6 @@
+abstract final class AppIcons {
+  static const String _basePath = 'assets/icons/';
+  static const String _extension = '.svg';
+
+  static const String arrowLeft = '${_basePath}arrowLeft$_extension';
+}
