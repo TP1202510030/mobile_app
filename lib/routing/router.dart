c40d24b3d426c7561803cd756bdf06b496a45747
alanegd <alangalavisd@gmail.com>
2025-04-28 11:48:50 -0500

    feat: Add custom transition to router

--- lib/routing/router.dart
+++ lib/routing/router.dart
@@ -1,18 +1,48 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:mobile_app/ui/auth/login/view_models/login_viewmodel.dart';
+import 'package:mobile_app/ui/auth/login/widgets/login_screen.dart';
 import 'package:mobile_app/ui/home/view_models/home_viewmodel.dart';
 
 import '../ui/home/widgets/home_screen.dart';
 import 'routes.dart';
 
-/// Top go_router entry point.
+CustomTransitionPage<T> buildPageWithoutAnimation<T>({
+  required BuildContext context,
+  required GoRouterState state,
+  required Widget child,
+}) {
+  return CustomTransitionPage<T>(
+      key: state.pageKey,
+      child: child,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        return child;
+      });
+}
+
 GoRouter router() => GoRouter(
       initialLocation: Routes.home,
       routes: [
         GoRoute(
           path: Routes.home,
-          builder: (context, state) {
+          pageBuilder: (context, state) {
             final viewModel = HomeViewModel();
-            return HomeScreen(viewModel: viewModel);
+            return buildPageWithoutAnimation<void>(
+              context: context,
+              state: state,
+              child: HomeScreen(viewModel: viewModel),
+            );
+          },
+        ),
+        GoRoute(
+          path: Routes.login,
+          pageBuilder: (context, state) {
+            final viewModel = LoginViewModel();
+            return buildPageWithoutAnimation(
+              context: context,
+              state: state,
+              child: LoginScreen(viewModel: viewModel),
+            );
           },
         ),
       ],
