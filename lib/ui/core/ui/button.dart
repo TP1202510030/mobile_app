a3e49044a4e6ac15708f73bb6addee3de35b9cc1
alanegd <alangalavisd@gmail.com>
2025-04-29 14:12:36 -0500

    chore: Update custom button component
    Custom button now accepts widgets as a child

--- lib/ui/core/ui/button.dart
+++ lib/ui/core/ui/button.dart
@@ -4,28 +4,22 @@ class CustomButton extends StatelessWidget {
   const CustomButton({super.key, required this.onTap, required this.child});
 
   final VoidCallback onTap;
-  final String child;
+  final Widget child;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
       child: ElevatedButton(
-        onPressed: onTap,
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Theme.of(context).colorScheme.primary,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(4.0),
+          onPressed: onTap,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(4.0),
+            ),
+            padding: const EdgeInsets.symmetric(vertical: 12.0),
           ),
-          padding: const EdgeInsets.symmetric(vertical: 12.0),
-        ),
-        child: Text(
-          child,
-          style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                color: Theme.of(context).colorScheme.onPrimary,
-              ),
-        ),
-      ),
+          child: child),
     );
   }
 }
