ea5fa9a19e823f7f27d72fa837a0692910a72e45
Alan Galavis <alangalavisd@gmail.com>
2025-05-28 00:29:03 -0500

    feat: Add horizontal option list widget

---
+++ lib/ui/core/ui/horizontal_option_list.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class HorizontalOptionList extends StatefulWidget {
+  final List<OptionItemData> options;
+
+  const HorizontalOptionList({super.key, required this.options});
+
+  @override
+  State<HorizontalOptionList> createState() => _HorizontalOptionListState();
+}
+
+class _HorizontalOptionListState extends State<HorizontalOptionList> {
+  int selectedIndex = 0;
+
+  void _onOptionTap(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+    widget.options[index].onTap?.call();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        padding: const EdgeInsets.symmetric(horizontal: 6.0),
+        itemCount: widget.options.length,
+        separatorBuilder: (_, __) => const SizedBox(width: 32),
+        itemBuilder: (context, index) {
+          final option = widget.options[index];
+          final isSelected = index == selectedIndex;
+
+          return GestureDetector(
+            onTap: () => _onOptionTap(index),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    color: isSelected
+                        ? Theme.of(context).colorScheme.primary
+                        : Colors.transparent,
+                    width: 3,
+                  ),
+                ),
+              ),
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    option.iconPath,
+                    width: 32,
+                    height: 32,
+                    colorFilter: ColorFilter.mode(
+                      Theme.of(context).colorScheme.onSurface,
+                      BlendMode.srcIn,
+                    ),
+                  ),
+                  const SizedBox(width: 4),
+                  Text(
+                    option.title,
+                    style: isSelected
+                        ? Theme.of(context)
+                            .textTheme
+                            .bodyMedium
+                            ?.copyWith(fontWeight: FontWeight.bold)
+                        : Theme.of(context).textTheme.bodyMedium,
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+
+class OptionItemData {
+  final String title;
+  final String iconPath;
+  final VoidCallback? onTap;
+
+  OptionItemData({
+    required this.title,
+    required this.iconPath,
+    this.onTap,
+  });
+}
+
