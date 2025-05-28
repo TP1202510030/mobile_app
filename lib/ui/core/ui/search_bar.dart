b4d2273b6043be1a9e6b8a0e6b58c46b5dd472e6
Alan Galavis <alangalavisd@gmail.com>
2025-05-27 23:55:07 -0500

    feat: Add custom search bar widget

---
+++ lib/ui/core/ui/search_bar.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:mobile_app/ui/core/themes/icons.dart';
+
+class CustomSearchBar extends StatefulWidget {
+  final String hintText;
+  final ValueChanged<String> onChanged;
+  final TextEditingController? controller;
+  final VoidCallback? onClear;
+
+  const CustomSearchBar({
+    super.key,
+    required this.hintText,
+    required this.onChanged,
+    this.controller,
+    this.onClear,
+  });
+
+  @override
+  State<CustomSearchBar> createState() => _CustomSearchBarState();
+}
+
+class _CustomSearchBarState extends State<CustomSearchBar> {
+  late final TextEditingController _controller;
+  bool _showClear = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = widget.controller ?? TextEditingController();
+    _showClear = _controller.text.isNotEmpty;
+
+    _controller.addListener(() {
+      final hasText = _controller.text.isNotEmpty;
+      if (hasText != _showClear) {
+        setState(() {
+          _showClear = hasText;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    if (widget.controller == null) {
+      _controller.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return TextField(
+      style: Theme.of(context).textTheme.bodyMedium,
+      controller: _controller,
+      onChanged: widget.onChanged,
+      decoration: InputDecoration(
+        contentPadding:
+            const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        hintText: widget.hintText,
+        prefixIcon: SvgPicture.asset(
+          AppIcons.search,
+          width: 32.0,
+          height: 32.0,
+          colorFilter: ColorFilter.mode(
+            Theme.of(context).colorScheme.onSurface,
+            BlendMode.srcIn,
+          ),
+        ),
+        suffixIcon: _showClear
+            ? IconButton(
+                icon: const Icon(Icons.clear),
+                onPressed: () {
+                  _controller.clear();
+                  widget.onClear?.call();
+                  setState(() => _showClear = false);
+                },
+              )
+            : null,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(4),
+          borderSide: BorderSide(color: theme.colorScheme.outline, width: 1.5),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(4),
+          borderSide: BorderSide(color: theme.colorScheme.outline, width: 1.5),
+        ),
+      ),
+    );
+  }
+}
